c33424c9821a63a4a1ba84d747a88d3e6f7d1e9a
Tiago de Tarso <ttrgoncalves@gmail.com>
2023-10-02 14:02:36 -0300

    02-10-2023-14:02

--- src/formItens/Select.jsx
+++ src/formItens/Select.jsx
@@ -7,7 +7,9 @@ export default function Select ({ name, text, size, ops, handleOnChange }) {
 
     if (Array.isArray(ops)) {
         options = ops.map(op => (
-            <option className={styles.options} key={op.name} value={op.value}>{op.name}</option>
+            typeof op === 'object' ?
+            <option className={styles.options} key={op.name} value={op.value}>{op.name}</option>:
+            <option className={styles.options} key={op} value={op}>{op}</option>
         ))
     }
 
